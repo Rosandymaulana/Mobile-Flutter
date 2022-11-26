65e3be1649470151fc97e4c196185b687cc55b3b
Rosandymaulana <rosandy1118@gmail.com>
2022-11-26 23:21:42 +0700

    2.6.1 Container

--- basic_flutter_application/lib/main.dart
+++ basic_flutter_application/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contoh Date Picker',
-      home: MyHomePage(title: 'Contoh Date Picker'),
+      home: ImageTransformSample(),
     );
   }
 }
@@ -77,6 +77,99 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+}
+
+class ButtonWithContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("Contoh Button")),
+          // ignore: avoid_unnecessary_containers
+          body: Container(
+            margin: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                textStyle: const TextStyle(color: Colors.white),
+              ),
+              onPressed: () {},
+              child: const Text("Button", style: TextStyle(fontSize: 20)),
+            ),
+          )),
+    );
+  }
+}
+
+class BottomAlignSample extends StatelessWidget {
+  const BottomAlignSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh Aligment")),
+        body: Container(
+          alignment: Alignment.bottomCenter,
+          child: const Text(
+            'Semangat Belajar',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorContainerSample extends StatelessWidget {
+  const ColorContainerSample({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("Contoh Color")),
+          body: Container(
+              padding: const EdgeInsets.only(left: 20),
+              margin: const EdgeInsets.all(20),
+              height: 99,
+              width: 250,
+              alignment: Alignment.topCenter,
+              color: Colors.orange,
+              child: const Text(
+                'Ayo Belajar Flutter',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ))),
+    );
+  }
+}
+
+class ImageTransformSample extends StatelessWidget {
+  const ImageTransformSample({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("Contoh Transform")),
+          body: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://images.unsplash.com/photo-1555677284-6a6f971638e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+                fit: BoxFit.fitHeight,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+            ),
+            height: 200,
+            width: 300,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+          )),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
