a1bc65cb2ba0200445ac2c29a388ccf6e33010b4
Rosandymaulana <rosandy1118@gmail.com>
2022-11-26 23:34:53 +0700

    2.6.4 ListView

--- basic_flutter_application/lib/main.dart
+++ basic_flutter_application/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contoh Date Picker',
-      home: StackWidgetSample(),
+      home: ListViewSample(),
     );
   }
 }
@@ -237,6 +237,50 @@ class StackWidgetSample extends StatelessWidget {
                 color: Colors.deepPurple,
                 alignment: Alignment.bottomCenter,
                 child: const Text("Tiga",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 200.0,
+                width: 200.0,
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class ListViewSample extends StatelessWidget {
+  const ListViewSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Contoh List Widget"),
+          ),
+          body: ListView(
+            children: <Widget>[
+              Container(
+                color: Colors.green,
+                alignment: Alignment.topLeft,
+                child: const Text(
+                    "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+              ),
+              Container(
+                color: Colors.red,
+                alignment: Alignment.topLeft,
+                child: const Text(
+                    "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 400.0,
+                width: 300.0,
+              ),
+              Container(
+                color: Colors.deepPurple,
+                alignment: Alignment.bottomCenter,
+                child: const Text(
+                    "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll",
                     style: TextStyle(fontSize: 30, color: Colors.white)),
                 height: 200.0,
                 width: 200.0,
