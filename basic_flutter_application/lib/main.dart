732c783796470e7ecf4664c809e3ca5fea5131da
Rosandymaulana <rosandy1118@gmail.com>
2022-11-26 23:39:54 +0700

    2.6.5 GridView

--- basic_flutter_application/lib/main.dart
+++ basic_flutter_application/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contoh Date Picker',
-      home: ListViewSample(),
+      home: GridViewSample(),
     );
   }
 }
@@ -281,6 +281,71 @@ class ListViewSample extends StatelessWidget {
                 alignment: Alignment.bottomCenter,
                 child: const Text(
                     "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 200.0,
+                width: 200.0,
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class GridViewSample extends StatelessWidget {
+  const GridViewSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Contoh GridView Widget"),
+          ),
+          body: GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(
+                color: Colors.green,
+                alignment: Alignment.center,
+                child: const Text("1",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+              ),
+              Container(
+                color: Colors.red,
+                alignment: Alignment.center,
+                child: const Text("2",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 400.0,
+                width: 300.0,
+              ),
+              Container(
+                color: Colors.deepPurple,
+                alignment: Alignment.center,
+                child: const Text("3",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 200.0,
+                width: 200.0,
+              ),
+              Container(
+                color: Colors.blueGrey,
+                alignment: Alignment.center,
+                child: const Text("4",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 200.0,
+                width: 200.0,
+              ),
+              Container(
+                color: Colors.cyan[800],
+                alignment: Alignment.center,
+                child: const Text("5",
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                height: 200.0,
+                width: 200.0,
+              ),
+              Container(
+                color: Colors.yellow[800],
+                alignment: Alignment.center,
+                child: const Text("6",
                     style: TextStyle(fontSize: 30, color: Colors.white)),
                 height: 200.0,
                 width: 200.0,
